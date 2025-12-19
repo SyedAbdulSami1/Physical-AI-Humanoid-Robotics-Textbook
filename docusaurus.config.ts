9704fd9e52cd9b4b58188f5905bb1b9cda1a8368
SyedAbdulSami1 <samiwpp@gmail.com>
2025-12-19 06:43:59 +0500

    correction in docusaurus

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,8 @@ const config: Config = {
   url: 'https://SyedAbdulSami1.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Physical-AI-Humanoid-Robotics-Textbook/',
+  baseUrl: '/',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   organizationName: 'SyedAbdulSami1', // Usually your GitHub org/user name.
